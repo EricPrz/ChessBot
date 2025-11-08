8d6d50978711c31ac71e47cd3d8405b05dd32ab1
eric <neryo19media@gmail.com>
2025-11-08 13:02:15 +0100

    Added Board + Pieces finding function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <chrono>
+#include <cstddef>
+#include <cstdio>
+#include <unordered_map>
 #include <iostream>
+#include <ostream>
+#include <stack>
 #include <string>
 #include <thread>
 #include <vector>
@@ -27,34 +32,93 @@ void print_smth(){
     }
 }
 
-enum Turn { white, black };
+const int SIZE = 8;
+
+enum Color { white, black };
+enum PieceType { Rook, Bishop, Knight, Queen, King, Pawn, none };
+enum MoveType { Illegal, Move, Capture, Enroque, Promocion };
+enum PieceLetter {p, r, b, n, k, q, P, R, B, N, K, Q};
+
+struct Piece {
+    PieceType piece;
+    Color color;
+
+    Piece(): piece(Pawn), color(black) {}
+    Piece(PieceType p, Color c): piece(p), color(c) {}
+};
+
+struct Position {
+    int x;
+    int y;
+
+    Position(int x, int y): x(x), y(y) {}
+};
 
 struct Board {
-	std::vector<std::vector<char>> board(8, std::vector<char>(8));
+    std::vector<std::vector<char>> board;
+};
 
-class Game{
-private:
-	Board board = {
-		{r, b, n, q, k, n, b, r},
-		{p, p, p, p, p, p, p, p},
-		{., ., ., ., ., ., ., .},
-		{., ., ., ., ., ., ., .},
-		{., ., ., ., ., ., ., .},
-		{., ., ., ., ., ., ., .},
-		{P, P, P, P, P, P, P, P},
-		{R, B, N, Q, K, N, B, R}
-	};
-	Turn turn = white;
+std::unordered_map<char, Piece> piece_to_board = {
+    {'p', Piece(Pawn, black)},
+    {'b', Piece(Bishop, black)},
+    {'r', Piece(Rook, black)},
+    {'n', Piece(Knight, black)},
+    {'q', Piece(Queen, black)},
+    {'k', Piece(King, black)},
 
+    {'_', Piece(none, white)},
 
+    {'P', Piece(Pawn, white)},
+    {'B', Piece(Bishop, white)},
+    {'R', Piece(Rook, white)},
+    {'N', Piece(Knight, white)},
+    {'Q', Piece(Queen, white)},
+    {'K', Piece(King, white)}
+};
+
+Piece piece_at(Position pos, Board board){
+    char piece = board.board.at(pos.y).at(pos.x);
+
+    return piece_to_board[piece];
 }
 
-get_all_moves(board)
+class Game {
+private:
+    Board board;
+    Color turn;
+    std::stack<std::string> moves;
+
+
+public:
+    Game(){
+	board = {{
+		{'r', 'b', 'n', 'q', 'k', 'n', 'b', 'r'},
+		{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'_', '_', '_', '_', '_', '_', '_', '_'},
+		{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+		{'R', 'B', 'N', 'Q', 'K', 'N', 'B', 'R'}
+	}};
+
+	turn = white;
+    }
+
+    Board get_board(){
+	return board;
+    }
+};
+
 
 
 
 
 int main(){
+    Game game;
+    Piece p = piece_at(Position(0, 2), game.get_board());
+
+    std::cout << p.piece << std::endl;
 
     std::thread m(read_smth);
     std::thread t(print_smth);
